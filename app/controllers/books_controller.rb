20665e8fb0238e052a690d7d7ee508b044144061
EC2 Default User <ec2-user@ip-172-31-5-181.ec2.internal>
2022-02-21 16:34:36 +0000

    [Finish]5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @book = Book.new
     @books = Book.all
@@ -53,6 +54,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :image, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   validates :name, presence: true
-  validates :name, length: {minimum: 2, maximum: 10}
+  validates :name, uniqueness: true
+  validates :name, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
 
   def get_profile_image
